26db99abce473ed1c24d110f51cd9beac66354f7
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-23 15:24:25 +0300

    Initial Commit

---
+++ Weather-DVT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather-DVT
+//
+//  Created by Sylvan  on 23/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather-DVT/Weather_DVTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DVTApp.swift
+//  Weather-DVT
+//
+//  Created by Sylvan  on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DVTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
