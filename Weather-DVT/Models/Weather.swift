74a0702296ce6ef513ef518d0f7a7b0b79471604
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-25 17:09:43 +0300

    vm: update the Weather model to include some computed properties

--- Weather-DVT/Models/Weather.swift
+++ Weather-DVT/Models/Weather.swift
@@ -6,12 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Weather: Identifiable {
     enum Condition {
         case sunny
         case rainy
         case cloudy
+
+        var text: String {
+            switch self {
+            case .sunny: return "SUNNY"
+            case .rainy: return "RAINY"
+            case .cloudy: return "CLOUDY"
+            }
+        }
+
+        var icon: String {
+            switch self {
+            case .sunny: return "clear"
+            case .rainy: return "rain"
+            case .cloudy: return "partlysunny"
+            }
+        }
+
+        var backgroundColor: Color {
+            switch self {
+            case .sunny: return .sunny
+            case .rainy: return .rainy
+            case .cloudy: return .cloudy
+            }
+        }
+
+        func backgroundImage(base: String = "forest_") -> String {
+            switch self {
+            case .sunny: return base + "sunny"
+            case .rainy: return base + "rainy"
+            case .cloudy: return base + "cloudy"
+            }
+        }
     }
 
     let id = UUID()
