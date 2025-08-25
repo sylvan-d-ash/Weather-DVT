7291b4a837dc8067609ba7be927781ecfab8e2d1
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-25 20:12:30 +0300

    tab: show settings and locations views

---
+++ Weather-DVT/Scenes/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Weather-DVT
+//
+//  Created by Sylvan  on 25/08/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
