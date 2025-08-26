bf2a4c75ddd8ef8ef8f941b732e3506546d56d69
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-26 17:11:00 +0300

    search: add a service + model for searching the map for locations of interst

---
+++ Weather-DVT/Core/Services/MapSearchService.swift
@@ -0,0 +1,49 @@
+//
+//  MapSearchService.swift
+//  Weather-DVT
+//
+//  Created by Sylvan  on 26/08/2025.
+//
+
+import Foundation
+import MapKit
+
+protocol MapSearchService: AnyObject {
+    func search(for query: String) async -> Result<[LocationSearchResult], Error>
+}
+
+final class DefaultMapSearchService: MapSearchService {
+    func search(for query: String) async -> Result<[LocationSearchResult], any Error> {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        request.resultTypes = .pointOfInterest
+
+        do {
+            let response = try await MKLocalSearch(request: request).start()
+            let results = response.mapItems.compactMap { item -> LocationSearchResult? in
+                guard let name = item.name, let location = item.placemark.location else {
+                    return nil
+                }
+
+                var region = ""
+                if let administrativeArea = item.placemark.administrativeArea {
+                    region = administrativeArea
+                }
+                if let country = item.placemark.country {
+                    region = region.isEmpty ? country : ", \(country)"
+                }
+
+                return .init(
+                    name: name,
+                    region: region,
+                    coordinate: location.coordinate
+                )
+            }
+            return .success(results)
+        } catch {
+            // TODO: log this
+            print("[MAP] Error: \(error.localizedDescription)")
+            return .failure(error)
+        }
+    }
+}
