1d30272fb850f86aa4eeb54f827771403a9c6c07
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-28 20:41:56 +0300

    testing: WeatherViewModel

--- Weather-DVTTests/Weather_DVTTests.swift
+++ Weather-DVTTests/Weather_DVTTests.swift
@@ -8,9 +8,7 @@
 import Testing
 
 struct Weather_DVTTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
 }
