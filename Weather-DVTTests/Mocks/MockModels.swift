15db8c458a6443494f9873247d8134cf5544e540
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-30 16:35:01 +0300

    model-context: add unit tests for PersistenceService

--- Weather-DVTTests/Mocks/MockModels.swift
+++ Weather-DVTTests/Mocks/MockModels.swift
@@ -9,17 +9,7 @@ import Foundation
 @testable import Weather_DVT
 
 extension Weather {
-    static func mock() -> Weather {
-        .init(
-            date: .now,
-            main: "clear",
-            currentTempInCelcius: 25,
-            minTempInCelcius: 19,
-            maxTempInCelcius: 26
-        )
-    }
-
-    static func mock(main: String?) -> Weather {
+    static func mock(main: String? = "clear") -> Weather {
         .init(
             date: .now,
             main: main,
@@ -49,12 +39,50 @@ extension Forecast {
 }
 
 extension CachedLocation {
-    static func mock(name: String) -> CachedLocation {
+    static func mock(id: UUID = UUID(), name: String, isCurrent: Bool = false) -> CachedLocation {
         .init(
+            id: id,
             name: name,
             region: "",
             latitude: 1,
-            longitude: 1
+            longitude: 1,
+            isCurrentUserLocation: isCurrent
+        )
+    }
+}
+
+extension WeatherLocation {
+    static func mock(kind: WeatherLocation.Kind) -> WeatherLocation {
+        .init(
+            id: UUID().uuidString,
+            name: "Test",
+            region: "",
+            latitude: 1,
+            longitude: 1,
+            kind: kind
+        )
+    }
+}
+
+extension CachedCurrentWeather {
+    static func mock() -> CachedCurrentWeather {
+        .init(
+            currentTempCelcius: 20,
+            minTempCelcius: 18,
+            maxTempCelcius: 23,
+            main: "rainy"
+        )
+    }
+}
+
+extension CachedForecastWeather {
+    static func mock() -> CachedForecastWeather {
+        .init(
+            currentTempCelcius: 19,
+            minTempCelcius: 15,
+            maxTempCelcius: 21,
+            main: "clouds",
+            date: .now
         )
     }
 }
