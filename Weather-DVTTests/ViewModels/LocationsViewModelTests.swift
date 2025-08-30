d44e932bdf6204df3780304237ed44d2b8a9d954
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-30 15:48:45 +0300

    model-context: implement addLocations and deleteLocations unit tests for Locations-ViewModel

--- Weather-DVTTests/ViewModels/LocationsViewModelTests.swift
+++ Weather-DVTTests/ViewModels/LocationsViewModelTests.swift
@@ -14,27 +14,24 @@ import SwiftData
 struct LocationsViewModelTests {
     var sut: LocationsView.ViewModel!
     var searchService: MockMapSearchService!
-    var modelContext: ModelContext!
+    var modelContext: MockModelContext!
     let location = SearchLocation(name: "Nairobi", region: "", coordinate: .init(latitude: 1, longitude: 1))
 
     init() {
-        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try! ModelContainer(for: CachedLocation.self, configurations: config)
-        modelContext = container.mainContext
-
+        modelContext = MockModelContext()
         searchService = MockMapSearchService()
         sut = .init(mapService: searchService, modelContext: modelContext)
     }
 
     @Test("empty search")
-    func emptySearch() {
+    func testEmptySearch() {
         sut.searchText = ""
         #expect(sut.searchResults.isEmpty)
         #expect(sut.errorMessage == nil)
     }
 
     @Test("search populates search results")
-    func searchPopulatesSearchResults() async {
+    func testSearchPopulatesSearchResults() async {
         searchService.searchResults = .success([location])
 
         let query = "Nai"
@@ -51,7 +48,7 @@ struct LocationsViewModelTests {
     }
 
     @Test("search sets error message on failure")
-    func searchSetsErrorMessageOnFailure() async {
+    func testSearchSetsErrorMessageOnFailure() async {
         searchService.searchResults = .failure(MockTestError.dummyError)
 
         let query = "Invalid"
@@ -65,7 +62,7 @@ struct LocationsViewModelTests {
     }
 
     @Test("navigate to result")
-    func navigateToResult() {
+    func testNavigateToResult() {
         #expect(sut.path.isEmpty, "Precondition: path should be empty.")
 
         sut.navigateToSearchResults(for: location)
@@ -74,7 +71,7 @@ struct LocationsViewModelTests {
     }
 
     @Test("navigation appends to exising path")
-    func navigationAppendsToExisingPath() {
+    func testNavigationAppendsToExisingPath() {
         let location2 = SearchLocation(name: "Mombasa", region: "", coordinate: .init(latitude: 2, longitude: 2))
         #expect(sut.path.isEmpty, "Precondition: path should be empty.")
 
@@ -87,21 +84,84 @@ struct LocationsViewModelTests {
         #expect(sut.path.count == 2, "Path should contain 2 elements")
     }
 
-    @Test("add location creates a new database entry")
-    func addLocationCreatesANewDatabaseEntry() async {
-        // TODO: test currently fails when trying to fetch from context
-//        sut.addLocation(location)
-//
-//        let descriptor = FetchDescriptor<CachedLocation>()
-//        let savedLocations = try! modelContext.fetch(descriptor)
-//
-//        #expect(savedLocations.count == 1)
-//        #expect(savedLocations.first?.name == "Nairobi")
-//        #expect(sut.searchText.isEmpty, "Search text should be cleared after adding a location")
+    @Test("add location creates a new unique database entry")
+    func testAddLocationWhenUniqueEntry() {
+        modelContext.mockCountResult = 0
+        sut.searchText = "Nair"
+
+        sut.addLocation(location)
+
+        let inserted = modelContext.getInsertedObject(ofType: CachedLocation.self)
+        #expect(sut.searchText == "", "Search text should be reset")
+        #expect(sut.errorMessage == nil, "Error message should be nil on success")
+        #expect(modelContext.didCallFetchCount)
+        #expect(modelContext.didCallInsert)
+        #expect(modelContext.didCallSave)
+        #expect(inserted != nil, "A CachedLocation object should be inserted")
+        #expect(inserted?.name == location.name)
     }
 
-    @Test("delete location")
-    func deleteLocation() async {
-        // TODO:
+    @Test("add location does not create a duplicate")
+    func testAddLocationWhenDuplicate() async {
+        modelContext.mockCountResult = 1
+        sut.searchText = "Nair"
+
+        sut.addLocation(location)
+
+        #expect(sut.searchText == "", "Search text should be reset")
+        #expect(sut.errorMessage == nil, "Error message should be nil regardless")
+        #expect(modelContext.didCallFetchCount)
+        #expect(modelContext.didCallInsert == false)
+        #expect(modelContext.didCallSave == false)
+        #expect(modelContext.insertedObjects.isEmpty, "No object should be created when a duplicate is found")
+    }
+
+    @Test("add location sets error message when database action fails")
+    func testAddLocationWhenDatabaseFails() {
+        modelContext.shouldThrowOnFetchCountError = true
+        sut.searchText = "Nair"
+
+        sut.addLocation(location)
+
+        #expect(sut.searchText == "Nair")
+        #expect(sut.errorMessage != nil)
+        #expect(modelContext.didCallFetchCount)
+        #expect(modelContext.didCallInsert == false)
+        #expect(modelContext.didCallSave == false)
+        #expect(modelContext.insertedObjects.isEmpty)
+    }
+
+    @Test("delete locations removes the correct items")
+    func testDeleteLocationsSuccess() async {
+        let allLocations = [
+            CachedLocation.mock(name: "Nairobi"),
+            CachedLocation.mock(name: "Mumbai"),
+            CachedLocation.mock(name: "Mombasa"),
+        ]
+        let offsets: IndexSet = [0, 2]
+
+        sut.deleteLocations(at: offsets, from: allLocations)
+
+        let deletedNames = modelContext.deleteObjects
+            .compactMap { $0 as? CachedLocation }
+            .map { $0.name }
+        #expect(modelContext.didCallDelete)
+        #expect(modelContext.didCallSave)
+        #expect(deletedNames == ["Nairobi", "Mombasa"])
+        #expect(modelContext.deleteObjects.count == 2)
+        #expect(sut.errorMessage == nil)
+    }
+
+    @Test("delete locations sets error message when database action fails")
+    func testDeleteLocationsFail() {
+        modelContext.shouldThrowOnSaveError = true
+        
+        let offsets: IndexSet = [0]
+        let allLocations = [CachedLocation.mock(name: "Nairobi")]
+        sut.deleteLocations(at: offsets, from: allLocations)
+
+        #expect(modelContext.didCallDelete)
+        #expect(modelContext.didCallSave)
+        #expect(sut.errorMessage != nil)
     }
 }
